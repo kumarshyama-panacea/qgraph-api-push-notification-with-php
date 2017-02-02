5da39566d57b2be709c7b1d82a916c9d1b817103
kumarshyama-panacea <shyama@panaceatek.com>
2017-02-02 16:21:26 +0530

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -3,7 +3,7 @@
  * Here all common required details related to account will be stored
  */
 
-define('Token', 'a3c91c899b781b6b6e0cd0cbe5b02c0fc659ac29');
+define('Token', '');
 
 define('API_URL', 'https://api.qgraph.io/api/v2/');
 
